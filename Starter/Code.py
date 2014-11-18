75a6701f9240c63c24851a86e331b58e06552a11
Tony Karimpumannil <40309@longroad.ca.uk>
2014-11-18 14:55:14 +0000

    push

--- Starter/Code.py
+++ Starter/Code.py
@@ -1,4 +1,6 @@
-
+#Tony K.
+#18/11/2014
+#Functions - Starter 
 
 
 
@@ -9,8 +11,8 @@ def calculate_basic_pay(hours,pay):
 
 #calculate overtime pay
 def calculate_overtime_pay(hours,pay):
+    basic_pay = hour * pay
     overtime_pay = (hours-40)*(pay*1.5)
-    return overtime_pay
-
-
+    total_pay_and_overtime = basic_pay + overtime_pay
+    return total_pay_and_overtime
     
