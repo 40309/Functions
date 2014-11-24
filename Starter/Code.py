ab5032c39a0704d3fc14760fa24e1d457bb43fbb
Tony Karimpumannil <40309@longroad.ca.uk>
2014-11-24 12:13:59 +0000

    Push

--- Starter/Code.py
+++ Starter/Code.py
@@ -6,7 +6,7 @@
 
 #calculate basic pay
 def calculate_basic_pay(hours,pay):
-    basic_pay = hours*pay
+    basic_pay = hours * pay
     return basic_pay
 
 #calculate overtime pay
@@ -18,18 +18,26 @@ def calculate_overtime_pay(hours,pay):
 
 #def calculate total pay
 def calculate_toal_pay(hours,pay):
-    if hours <=0:
+    if hours <=40:
         total_pay = calculate_basic_pay(hours,pay)
     else:
-        calculate_overtime_pay(hours,pay)
+        total_pay = calculate_overtime_pay(hours,pay)
     return total_pay
 
-#Get Work details
+#Get Work details, Good
 def work_details():
     hours = int(input("Please enter the amount of hours you have worked: "))
-    pay = float(input("Please enter your pay rate: "))
+    pay = float(input("Please enter your pay rate:£ "))
     return hours,pay
 
 #Display Pay
-def display_total_pay(total):
-    print("You have earned £{0} is month".format(total))
+def display_total_pay(total_pay):
+    print("You have earned £{0} is month".format(total_pay))
+
+#Final Stage: Calculate Pay
+def calculate_pay():
+    hours,pay = work_details()
+    total_pay = calculate_toal_pay(hours,pay)
+    display_total_pay(total_pay)
+
+
